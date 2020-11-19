07582fd2c08b33ae22ce4c2747470c3676aa22a1
MH4454 <68453774+MH4454@users.noreply.github.com>
2020-11-18 23:28:59 -0500

    Updated HTML and CSS

--- Main/script.js
+++ Main/script.js
@@ -1,20 +1,20 @@
+// Displays the current date
+let today = new Date().toLocaleDateString();
+$("#today").text(today)
+// Gets the user city input, if not entered yet then grabs from local storage.
 let userCityInput = $("#search-value").val(); // needs to be replaced with local storage last city
 const key = "73cfa05aa468e095076d7f42999784f7";
 
 //Gets city by user input
 $("#search-button").on("click", function(){
-  let userCityInput = $("#search-value").val();
-  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCityInput + "&appid=" + key;
   userCityInput = $("#search-value").val();
+  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCityInput + "&units=imperial&appid=" + key;
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
-
-  
-  
 });
 
 })
